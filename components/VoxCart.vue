42af1f23283779f6baae0be5295d48662c00d69d
Trinmar Boado <timi.boado@gmail.com>
2023-05-24 11:08:27 +0800

    draft cart

---
+++ components/VoxCart.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="cart-component">
+    <div class="cart-header row justify-center align-center">
+      <div class="title">
+        Your Cart (2)
+      </div>
+      <span class="flex-grow-1" />
+      <div class="button-container">
+        <v-btn color="black" icon @click="$emit('close')">
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+      </div>
+    </div>
+    <div class="cart-items">
+      <v-card
+        class="mx-auto no-gutter no-padding no-shadow"
+      >
+        <v-list-item three-line>
+          <v-list-item-avatar
+            tile
+            size="80"
+            color="grey"
+          />
+          <v-list-item-content>
+            <v-list-item-title class="text-h5 mb-1">
+              Headline 5
+            </v-list-item-title>
+            <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            outlined
+            rounded
+            text
+          >
+            Button
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </div>
+    <div class="cart-checkout">
+      checkout
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.cart-component {
+  padding: 32px 32px 24px;
+}
+
+.cart-header {
+
+  border-bottom: 1px solid rgba(26, 26, 26, 0.1);
+  padding: 0px 0px 24px;
+  gap: 24px;
+}
+
+.title {
+  font-family: 'Instrument Sans';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 22px;
+  line-height: 27px;
+}
+</style>
--- components/VoxFilter.vue
+++ components/VoxFilter.vue
@@ -1,17 +1,18 @@
 <template>
-  <v-col cols="12" class="desktop-padding no-padding">
-    <!-- {{ $store.state.filter }} -->
-    <v-tabs
-      :model-value="$store.state.filter.tab"
-      class="no-gutter"
-      color="deep-purple-accent-4"
-      align-tabs="center"
-      height="64px"
-    >
-      <v-tab v-for="cat in categories" :key="cat" :value="cat" height="64px" @click="setTab(cat)">
-        {{ cat }}
-      </v-tab>
-    </v-tabs>
+  <v-col cols="12" class="no-padding">
+    <div class="d-flex justify-center align-center">
+      <v-tabs
+        :model-value="$store.state.filter.tab"
+        class="no-gutter max-width-1400"
+        color="deep-purple-accent-4"
+        align-tabs="center"
+        height="64px"
+      >
+        <v-tab v-for="cat in categories" :key="cat" :value="cat" height="64px" @click="setTab(cat)">
+          {{ cat }}
+        </v-tab>
+      </v-tabs>
+    </div>
   </v-col>
 </template>
 
@@ -25,19 +26,19 @@ export default {
     }
   },
   async fetch () {
-    const response = await this.$axios.get(
-      'https://fakestoreapi.com/products/categories'
-    )
+    const response = await this.$axios.get('https://fakestoreapi.com/products/categories')
     const categories = response.data
     this.categories = ['all', ...categories]
-  },
-  computed: {
-    cart () {
-      return this.$store.state.filter.tab
-    }
   },
   methods: {
     ...mapMutations('filter', ['setTab'])
   }
 }
 </script>
+
+<style scoped lang="scss">
+.center-tabs {
+  justify-content: center;
+  align-items: center;
+}
+</style>
