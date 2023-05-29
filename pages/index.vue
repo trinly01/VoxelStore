5cfbf5bda9511278e5fb597cd2a79a7f190877a0
Trinmar Boado <timi.boado@gmail.com>
2023-05-29 17:16:10 +0800

    Full SSR from products details
    The application should be fully server side rendered, currently the meta tags are coming from the server but not the product information in the grid

--- pages/index.vue
+++ pages/index.vue
@@ -9,16 +9,68 @@
 <script>
 export default {
   name: 'IndexPage',
+  asyncData ({ app }) {
+    return app.$axios.$get('https://fakestoreapi.com/products')
+      .then(data => ({ products: data }))
+      .catch((error) => {
+        window.console.error('Error fetching product data:', error)
+        return { products: [] }
+      })
+  },
   data () {
     return {
       products: []
     }
   },
-  async mounted () {
-    const response = await fetch('https://fakestoreapi.com/products')
-    const data = await response.json()
-    this.products = [...data]
-    window.console.log(data)
+  head () {
+    let title = 'Voxel Store - '
+    let description = 'Voxel Store - products all electronics jewelery me clothing'
+    let image = ''
+    let rating = ''
+
+    // Check if products data exists
+    if (this.products.length > 0) {
+      const firstProduct = this.products[0]
+      title = firstProduct.title
+      description = firstProduct.description
+      image = firstProduct.image
+      rating = firstProduct.rating.rate
+    }
+
+    return {
+      title,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: description
+        },
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: title
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: description
+        },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content: image
+        },
+        {
+          hid: 'og:rating',
+          property: 'og:rating',
+          content: rating
+        }
+        // Add more meta tags as needed
+      ]
+    }
+  },
+  mounted () {
+    window.console.log('products', this.products)
   }
 }
 </script>
